ae44f9bda6c0a35eeb1a759e63ad9551f531a8e5
Joaquin Miguel Garcia Sastre <jgarciasastre@twilio.com>
2021-12-06 01:33:03 +0100

    Add haskell language server to nix shell

--- shell.nix
+++ shell.nix
@@ -3,10 +3,12 @@ let
 in
   nixpkgs.mkShell {
     buildInputs = [
+      nixpkgs.haskell-language-server
       nixpkgs.haskellPackages.cabal-install
       nixpkgs.haskellPackages.regex-compat
       nixpkgs.ghc
       nixpkgs.ghcid
       nixpkgs.hlint
+      nixpkgs.ispell
     ];
   }
